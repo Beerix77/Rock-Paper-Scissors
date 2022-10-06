9af352571f92750242d48c5c1d43bd2c43f45026
Andrew Matysiak <andrewmatysiak@hotmail.com>
2022-10-06 11:53:39 +1100

    second commit Work In Progress

--- Choice.java
+++ Choice.java
@@ -1,4 +1,3 @@
-
 public class Choice {
-
-    }
+        int x = 1;
+}
--- Main.java
+++ Main.java
@@ -1,20 +1,31 @@
+import java.awt.*;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("*** Welcome to (R)OCK, (P)APER, (S)CISSORS !!! ***");
 
-        System.out.print("Enter your choice: ");
-        String choice = keyboard.nextLine();
-        if (!choice.equals("R") && !choice.equals("P") && !choice.equals("S")){
-            System.out.println("You must choose 'R', 'P' or 'S'");
+        String choice = "";
+
+        while (!choice.equals("R") && !choice.equals("P") && !choice.equals("S")) {
+            System.out.println("Please Enter your choice 'R', 'P' or 'S'");
+            choice = keyboard.nextLine();
         }
 
+        String myChoice = "";
+        if (choice.equals("R")){
+            myChoice = "Rock";}
+        else if (choice.equals("P")){
+            myChoice = "Paper";}
+        else {myChoice = "Scissors";}
 
 
-
-
+        System.out.println("You have chosen: " + myChoice);
+        Random javaChoice = new Random(3);
+        System.out.println("JAVA has chosen: " + javaChoice);
     }
-
 }
